9a47840cdded5988f247c72fba002f2ab435cca0
romarios1987 <romios047@gmail.com>
2018-11-07 22:01:20 +0200

    basic routing, views

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const app = express();
 
 const port = 5000;
 
+
+
+app.set('view engine', 'ejs');
+
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('INDEX');
+});
+
+// About Route
+app.get('/about', (req, res) => {
+  res.send('ABOUT');
+});
+
+
 app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
