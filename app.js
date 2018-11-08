97e0010fa8f9affa5a342cab26d63db85393083b
romarios1987 <romios047@gmail.com>
2018-11-08 19:55:24 +0200

    update views, add layout(bootstrap)

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 
 const app = express();
-
 const port = 5000;
 
 
+app.use(expressLayouts);
 
-app.set('view engine', 'ejs');
+app.set('layout', 'layouts/layout');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs'); //extension of views
 
 
 // Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX');
+    res.render('index', {title: 'Express'});
 });
 
 // About Route
 app.get('/about', (req, res) => {
-  res.send('ABOUT');
+    res.render('about');
 });
 
-
-app.listen(port, () =>{
-  console.log(`Server started on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
