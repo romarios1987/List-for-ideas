56005cc1075bf6b10eced651e234b45fde12f320
romarios1987 <romios047@gmail.com>
2018-11-08 22:47:46 +0200

    Add idea form, validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
@@ -15,12 +16,16 @@ require('./models/Idea');
 const Idea = mongoose.model('ideas');
 
 
-
 app.use(expressLayouts);
 
 app.set('layout', 'layouts/layout');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs'); //extension of views
+
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 // Index Route
@@ -32,6 +37,40 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
+
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+
+    let errors = [];
+
+    if (!req.body.title) {
+        errors.push({text: 'Please add title'})
+    }
+
+    if (!req.body.details) {
+        errors.push({text: 'Please add some details'})
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details,
+        })
+    } else {
+        res.send('passed');
+    }
+
+    // console.log(req.body);
+    // res.send('OK');
+});
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
