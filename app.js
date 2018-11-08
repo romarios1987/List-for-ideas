91524527c236e6f4a1e9d3b2937f921280fcc7f8
romarios1987 <romios047@gmail.com>
2018-11-08 20:45:47 +0200

    mongoose, create first model Idea

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 5000;
+
+// Connect to mongoose
+mongoose.connect('mongodb://Remi:remi111@ds147033.mlab.com:47033/facts', {useNewUrlParser: true})
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+// Load Idea Modal
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
 
 
 app.use(expressLayouts);
